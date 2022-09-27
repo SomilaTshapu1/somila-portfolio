684d871b4130a02404f97bced311fd478373c4c4
Somila Tshapu <82814273+SomilaTshapu1@users.noreply.github.com>
2022-09-27 11:30:02 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -74,3 +74,20 @@ function reset() {
   document.querySelector(".name-error").innerText = "";
 }
 
+//top button
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
