c80359a62eb47e753825dd2d2409d410bfd21e02
Somila Tshapu <82814273+SomilaTshapu1@users.noreply.github.com>
2022-09-27 11:18:38 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+$('.slider').slick({
+    arrows:false,
+    dots:true,
+    appendDots:'.slider-dots',
+    dotsClass:'dots'
+});
+
+
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+hamberger.addEventListener('click', function(){
+  mobileNav.classList.add('open');  
+});
+
+times.addEventListener('click', function(){
+    mobileNav.classList.remove('open');  
+});
+
+});
+
+//Contact
+const nameEl = document.querySelector("#name");
+const emailEl = document.querySelector("#email");
+const numberEl = document.querySelector("#umber");
+const companyNameEl = document.querySelector("#company-name");
+const messageEl = document.querySelector("#message");
+
+const form = document.querySelector("#submit-form");
+
+function checkValidations() {
+  let letters = /^[a-zA-Z\s]*$/;
+  const name = nameEl.value.trim();
+  const email = emailEl.value.trim();
+  const number = numberEl.value.trim();
+  const companyName = companyNameEl.value.trim();
+  const message = messageEl.value.trim();
+  if (name === "") {
+     document.querySelector(".name-error").classList.add("error");
+      document.querySelector(".name-error").innerText =
+        "Please fill out this field!";
+  } else {
+    if (!letters.test(name)) {
+      document.querySelector(".name-error").classList.add("error");
+      document.querySelector(".name-error").innerText =
+        "Please enter only characters!";
+    } else {
+      
+    }
+  }
+  if (email === "") {
+     document.querySelector(".name-error").classList.add("error");
+      document.querySelector(".name-error").innerText =
+        "Please fill out this field!";
+  } else {
+    if (!letters.test(name)) {
+      document.querySelector(".name-error").classList.add("error");
+      document.querySelector(".name-error").innerText =
+        "Please enter only characters!";
+    } else {
+      
+    }
+  }
+}
+
+function reset() {
+  nameEl = "";
+  emailEl = "";
+  numberEl
+  companyNameEl = "";
+  messageEl = "";
+  document.querySelector(".name-error").innerText = "";
+}
+
